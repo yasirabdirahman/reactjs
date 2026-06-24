cf3f6b722e2a82ba39c4cf9c22a7cb115d07eac7
yasirabdirahman <essaaptherahman@gmail.com>
2026-06-24 06:32:18 +0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import UserCard from './UserCard';
+
+
+const App = () =>{
+  return(
+<UserCard/>
+
+
+  //   <>
+  //     <Greeting />
+  //     <WelcomeMassage/>
+  //   </>
+  )
+}
+
+export default App;
---
+++ src/UserCard.jsx
@@ -0,0 +1,10 @@
+const UserCard = ()=> {
+    return(
+        <>
+            <h2>yasser</h2>
+            <p>reelvana@gmail</p>
+        </>
+    )
+}
+
+export default UserCard;
