042f76a1af9236d46643adb9afa49e7366c19557
yasirabdirahman <essaaptherahman@gmail.com>
2026-08-05 00:22:02 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-// App.jsx
-import React, { useState } from 'react';
-import CartContext from './CartContext';
-import ProductItem from './ProductItem';
-import CartSummary from './CartSummary';
+import React from 'react';
+import ShoppingCart from './exercise16/ShoppingCart';
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
-
-  const removeFromCart = (itemId) => {
-    setCartItems(cartItems.filter((item) => item.id !== itemId));
-  };
-
-  const value = { cartItems, addToCart, removeFromCart };
-
   return (
-    <CartContext.Provider value={value}>
-      <ProductItem itemId={1} itemName="Widget" price={19.99} />
-      <ProductItem itemId={2} itemName="Gadget" price={29.99} />
-      <CartSummary />
-    </CartContext.Provider>
+    <div>
+      <ShoppingCart />
+    </div>
   );
 }
 
